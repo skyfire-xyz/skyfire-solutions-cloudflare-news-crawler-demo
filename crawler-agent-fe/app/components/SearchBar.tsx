b1a31172ca8ff18b4d74cc835025399280a7ca25
skyfire-supreet <supreet@skyfire.xyz>
2025-12-11 14:29:56 -0500

    SKYK-985 implements api gateway + waf

--- crawler-agent-fe/app/components/SearchBar.tsx
+++ crawler-agent-fe/app/components/SearchBar.tsx
@@ -48,6 +48,7 @@ const suggestions: Suggestion[] = [
   { url: "https://skyfire.xyz", name: "Skyfire", type: "Unprotected" },
   { url: "https://mock-news-site.skyfire.xyz/", name: "MockNews", type: "Protected"},
   { url: "https://mock-news-cloudflare-api-gateway-demo.skyfire.xyz/", name: "MockNews (Cloudflare API Gateway)", type: "Protected"},
+  { url: "https://mock-news-cloudflare-api-gateway-waf-demo.skyfire.xyz/", name: "MockNews (Cloudflare API Gateway + WAF)", type: "Protected"},
   { url: "https://mock-news-cloudflare-cdn-demo.skyfire.xyz/", name: "MockNews (Cloudflare CDN)", type: "Protected"},
   { url: "https://mock-news-cloudflare-cdn-waf1-demo.skyfire.xyz/", name: "MockNews (Cloudflare CDN + WAF)", type: "Protected"}
 ]
